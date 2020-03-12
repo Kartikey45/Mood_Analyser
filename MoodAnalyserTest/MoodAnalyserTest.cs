2eafab03c3b70fe60f0d13e937edf9d51235acd6
Kartikey45 <singhkartikey45@gmail.com>
2020-03-12 18:32:31 +0530

    ADDED : TC-2.1 Given null mood should return happy

--- MoodAnalyserTest/MoodAnalyserTest.cs
+++ MoodAnalyserTest/MoodAnalyserTest.cs
@@ -2,22 +2,13 @@ using NUnit.Framework;
 using Mood_Analyser_Problem;
 
 namespace MoodAnalyserTest
-{
-    
+{   
     public class Tests
     {
         [Test]
         public void Test1()
         {
-            MoodAnalyser mood = new MoodAnalyser("I am in sad mood");
-            string value = mood.analyseMood();
-            Assert.AreEqual("sad", value);
-        }
-
-        [Test]
-        public void Test2()
-        {
-            MoodAnalyser mood = new MoodAnalyser("I am in happy mood");
+            MoodAnalyser mood = new MoodAnalyser(null);
             string value = mood.analyseMood();
             Assert.AreEqual("happy", value);
         }

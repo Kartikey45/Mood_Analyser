12ea1f637db7ec199223dfa211b3b6257c77e5f9
Kartikey45 <singhkartikey45@gmail.com>
2020-03-12 16:21:00 +0530

    ADDED : Welcome to Mood Analyser Problem

---
+++ Mood_Analyser_Problem/MoodAnalyser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mood_Analyser_Problem
+{
+    class MoodAnalyser
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyser");
+            Console.ReadKey();
+        }
+    }
+}

ad0397784c76e55edefbc3644018993b6bc07767
Kartikey45 <singhkartikey45@gmail.com>
2020-03-13 19:26:46 +0530

    ADDED : TC-3 Test case for Maximum of three strings

--- MaximumNumberTest/UnitTest1.cs
+++ MaximumNumberTest/UnitTest1.cs
@@ -20,5 +20,13 @@ namespace MaximumNumberTest
             float result = maximum.MaximumElement(1.2f, 3.2f, 2.2f);
             Assert.AreEqual(3.2f, result);
         }
+
+        [Test]
+        public void Test3()
+        {
+            MaximumNumber maximum = new MaximumNumber();
+            int result = maximum.maximumElement1("kartikey", "anurag", "abhijeet");
+            Assert.AreEqual(8, result);
+        }
     }
 }

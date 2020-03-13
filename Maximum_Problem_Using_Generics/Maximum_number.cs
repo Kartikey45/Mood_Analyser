73e519f0a9154f03489d25d919bc6ec7402087a4
Kartikey45 <singhkartikey45@gmail.com>
2020-03-13 10:47:31 +0530

    ADDED : Welcome to Generics

---
+++ Maximum_Problem_Using_Generics/Maximum_number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Maximum_Problem_Using_Generics
+{
+    public class Maximum_number
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics");
+            Console.ReadKey();
+        }
+    }
+}

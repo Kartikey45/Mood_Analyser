4adbaee82390a44cd01a69aaf3784cde17df313b
Kartikey45 <singhkartikey45@gmail.com>
2020-03-13 11:29:07 +0530

    ADDED : Find maximum of three integers

--- Maximum_Problem_Using_Generics/Maximum_number.cs
+++ Maximum_Problem_Using_Generics/Maximum_number.cs
@@ -4,9 +4,28 @@ namespace Maximum_Problem_Using_Generics
 {
     public class Maximum_number
     {
+        //MAIN METHOD
         public static void Main(string[] args)
         {
+            //ARRAY DECALRE
+            int[] integer = { 56, 32, 87 };
+
+            //VARIABLE INITIALIZE
+            int maximumInteger = 0;
+            int arrayLength = integer.Length;
+
+            //FIND MAXIMUM INTEGER
+            for (int index = 0 ; index < arrayLength ; index++)
+            {
+                if (integer[index] > maximumInteger)
+                {
+                    maximumInteger = integer[index];
+                }
+            }
+
+            //DISPLAY THE MAXIMUM INTEGER
             Console.WriteLine("Welcome to Generics");
+            Console.WriteLine("Maximum integer is : " + maximumInteger);
             Console.ReadKey();
         }
     }

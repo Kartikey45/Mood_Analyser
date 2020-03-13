009c4a151c0f1ff3ada6763706f3664b1061a43f
Kartikey45 <singhkartikey45@gmail.com>
2020-03-13 17:47:55 +0530

    ADDED : TC-1 Test case for Maximum of three integers

---
+++ MaximumNumberTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using Maximum_Problems_Using_Generic;
+
+namespace MaximumNumberTest
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            MaximumNumber maximum = new MaximumNumber();
+            int result = maximum.MaximumElement(1, 3, 2);
+            Assert.AreEqual(3, result);
+        }
+    }
+}

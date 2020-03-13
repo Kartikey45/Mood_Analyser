e454553925fc79aa8b49defb33b71497328bdb57
Kartikey45 <singhkartikey45@gmail.com>
2020-03-13 17:19:48 +0530

    ADDED : Welcome to Generics git add .!

---
+++ Maximum_Problems_Using_Generic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Maximum_Problems_Using_Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics");
+        }
+    }
+}

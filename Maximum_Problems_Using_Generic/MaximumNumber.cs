d73ddc3b49122314a5243ac75f95c1418c96b603
Kartikey45 <singhkartikey45@gmail.com>
2020-03-13 17:40:36 +0530

    ADDED : UC-1 Find maximum of three integers

--- Maximum_Problems_Using_Generic/MaximumNumber.cs
+++ Maximum_Problems_Using_Generic/MaximumNumber.cs
@@ -2,11 +2,34 @@
 
 namespace Maximum_Problems_Using_Generic
 {
-    class MaximumNumber
+    public class MaximumNumber
     {
-        static void Main(string[] args)
+        //MAIN METHOD
+        public int MaximumElement(int number1, int number2, int number3)
+        {
+            //ARRAY DECALRE
+            int[] integer = { number1, number2, number3 };
+
+            //VARIABLE INITIALIZE
+            int maximumInteger = 0;
+            int arrayLength = integer.Length;
+
+            //FIND MAXIMUM INTEGER
+            for (int index = 0; index < arrayLength; index++)
+            {
+                if (integer[index] > maximumInteger)
+                {
+                    maximumInteger = integer[index];
+                }
+            }
+            return maximumInteger;
+        }
+
+        //MAIN METHOD
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generics");
+            Console.ReadKey();
         }
     }
 }

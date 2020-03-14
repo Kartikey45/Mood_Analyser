a5c517a56701463a52ed5d9b42b8052bedc39a3b
Kartikey45 <singhkartikey45@gmail.com>
2020-03-14 10:48:01 +0530

    REFACTOR-1 : UC-3 Refactor all the 3 in one generic method and find the maximum element

--- Maximum_Problems_Using_Generic/MaximumNumber.cs
+++ Maximum_Problems_Using_Generic/MaximumNumber.cs
@@ -5,64 +5,19 @@ namespace Maximum_Problems_Using_Generic
 {
     public class MaximumNumber
     {
-        //METHOD FOR FINDING MAXIMUM INTEGER NUMBER
-        public int MaximumElement(int number1, int number2, int number3)
+        //METHOD FOR FINDING MAXIMUM ELEMENT
+        public anyDataType MaximumElement<anyDataType>(anyDataType element1, anyDataType element2, anyDataType element3)
         {
             //ARRAY DECALRE
-            int[] integer = { number1, number2, number3 };
+            anyDataType[] array = { element1, element2, element3 };
 
-            //VARIABLE INITIALIZE
-            int maximumInteger = 0;
-            int arrayLength = integer.Length;
-
-            //FIND MAXIMUM INTEGER
-            for (int index = 0; index < arrayLength; index++)
-            {
-                if (integer[index] > maximumInteger)
-                {
-                    maximumInteger = integer[index];
-                }
-            }
-            return maximumInteger;
+            //FIND MAXIMUM ELEMENT
+            anyDataType MaximumValue = array.Max();
+            return MaximumValue;
         }
-
-        //METHOD FOR FINDING MAXIMUM FLOAT NUMBER
-        public float MaximumElement(float number1, float number2, float number3)
-        {
-            //ARRAY DECALRE
-            float[] floatArray = { number1, number2, number3 };
-
-            //VARIABLE INITIALIZE
-            float maximumNumber = 0;
-            float arrayLength = floatArray.Length;
-
-            //FIND MAXIMUM INTEGER
-            for (int index = 0; index < arrayLength; index++)
-            {
-                if (floatArray[index] > maximumNumber)
-                {
-                    maximumNumber = floatArray[index];
-                }
-            }
-            return maximumNumber;
-        }
-
-        //METHOD FOR FINDING MAXIMUM STRING
-        public int maximumElement1(string element1, string element2, string element3)
-        {
-            int string1 = element1.Length;
-            int string2 = element2.Length;
-            int string3 = element3.Length;
-            int[] stringElement = { string1, string2, string3 };
-
-            //Find The Maximum Number In Array
-            int MaximumValue1 = stringElement.Max();
-            return MaximumValue1;
-        }
-
 
         //MAIN METHOD
-        public static void Main(string[] args)
+        public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Generics");
             Console.ReadKey();
